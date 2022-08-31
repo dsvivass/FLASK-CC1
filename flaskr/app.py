f60fbf0c85a3244eda43abd66f5497dbfa0fb616
dsvivass <dsvivass@unal.edu.co>
2022-08-30 23:39:16 -0500

    Serializacion

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,6 @@
 from flaskr import create_app
 from .models import db, Cancion, Album, Medio, Usuario
-from .models import AlbumSchema
+from .models import AlbumSchema, CancionSchema, UsuarioSchema
 
 app = create_app('default')
 
@@ -14,16 +14,16 @@ db.create_all()
 
 #Prueba
 with app.app_context():
-    # u = Usuario(nombre='Juan', contrasena='12345')
-    # a = Album(titulo='Prueba', anio=2020, descripcion='Prueba', medio=Medio.CD)
-    # c = Cancion(titulo='Prueba', minutos=1, segundos=30, interprete='Juan')
+    u = Usuario(nombre='Juan', contrasena='12345')
+    a = Album(titulo='Prueba', anio=2020, descripcion='Prueba', medio=Medio.CD)
+    c = Cancion(titulo='Prueba', minutos=1, segundos=30, interprete='Juan')
     
-    # u.albumes.append(a) 
-    # a.canciones.append(c)
+    u.albumes.append(a) 
+    a.canciones.append(c)
     
-    # db.session.add(u)
-    # db.session.add(c)
-    # db.session.commit()
+    db.session.add(u)
+    db.session.add(c)
+    db.session.commit()
     
     # print(Album.query.all())
     # print(Album.query.all()[0].canciones)
@@ -34,10 +34,11 @@ with app.app_context():
     # print(Cancion.query.all())
     
     album_schema = AlbumSchema()
-    a = Album(titulo='Prueba', anio=1999, descripcion='texto', medio=Medio.CD)
-    db.session.add(a)
-    db.session.commit()
+    cancion_schema = CancionSchema()
+    usuario_schema = UsuarioSchema()
     
     print(Album.query.all())
     
     print([album_schema.dumps(album) for album in Album.query.all()])
+    print([cancion_schema.dumps(cancion) for cancion in Cancion.query.all()])
+    print([usuario_schema.dumps(usuario) for usuario in Usuario.query.all()])
