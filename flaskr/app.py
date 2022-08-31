3ed63b60f538fd8f33b6b0de61ebd9cbf57b2ef8
dsvivass <dsvivass@unal.edu.co>
2022-08-30 23:35:18 -0500

    Serializacion

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,6 @@
 from flaskr import create_app
-from .models import db, Cancion
+from .models import db, Cancion, Album, Medio, Usuario
+from .models import AlbumSchema
 
 app = create_app('default')
 
@@ -13,8 +14,30 @@ db.create_all()
 
 #Prueba
 with app.app_context():
-    c = Cancion(titulo='Prueba', minutos=2, segundos=25, interprete='Juan Pablo')
-    db.session.add(c)
+    # u = Usuario(nombre='Juan', contrasena='12345')
+    # a = Album(titulo='Prueba', anio=2020, descripcion='Prueba', medio=Medio.CD)
+    # c = Cancion(titulo='Prueba', minutos=1, segundos=30, interprete='Juan')
+    
+    # u.albumes.append(a) 
+    # a.canciones.append(c)
+    
+    # db.session.add(u)
+    # db.session.add(c)
+    # db.session.commit()
+    
+    # print(Album.query.all())
+    # print(Album.query.all()[0].canciones)
+    # print(Cancion.query.all())
+    
+    # db.session.delete(a)
+    # print(Album.query.all())
+    # print(Cancion.query.all())
+    
+    album_schema = AlbumSchema()
+    a = Album(titulo='Prueba', anio=1999, descripcion='texto', medio=Medio.CD)
+    db.session.add(a)
     db.session.commit()
     
-    print(Cancion.query.all())
+    print(Album.query.all())
+    
+    print([album_schema.dumps(album) for album in Album.query.all()])
--- flaskr/models/models.py
+++ flaskr/models/models.py
@@ -1,6 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+import enum
+from marshmallow import fields
+from sqlalchemy import Enum
 
 db = SQLAlchemy()
+
+albumes_canciones = db.Table('album_cancion',
+                             db.Column('album_id', db.Integer, db.ForeignKey(
+                                 'album.id'), primary_key=True),
+                             db.Column('cancion_id', db.Integer, db.ForeignKey(
+                                 'cancion.id'), primary_key=True)
+                             )
+
 
 class Cancion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,6 +20,75 @@ class Cancion(db.Model):
     minutos = db.Column(db.Integer)
     segundos = db.Column(db.Integer)
     interprete = db.Column(db.String(128))
-    
+    albumes = db.relationship(
+        'Album', secondary='album_cancion', back_populates='canciones')
+
     def __repr__(self):
         return f"{self.titulo} - {self.minutos} - {self.segundos} - {self.interprete}"
+
+
+class Medio(enum.Enum):
+    DISCO = 1
+    CASETE = 2
+    CD = 3
+
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    anio = db.Column(db.Integer)
+    descripcion = db.Column(db.String(128))
+    medio = db.Column(Enum(Medio))
+
+    usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+
+    canciones = db.relationship(
+        'Cancion', secondary='album_cancion', back_populates='albumes')
+
+    __table_args__ = (db.UniqueConstraint(
+        'usuario', 'titulo', name='titulo_unico_album'),)
+
+    def __repr__(self):
+        return f"{self.titulo} - {self.anio} - {self.medio}"
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(64))
+    contrasena = db.Column(db.String(32))
+
+    albumes = db.relationship('Album', cascade='all, delete, delete-orphan')
+
+    def __repr__(self):
+        return f"{self.nombre} - {self.contrasena}"
+
+
+class EnumADiccionario(fields.Field):
+    def _serialize(self, value, attr, obj, **kwargs):
+        if value is None:
+            return None
+
+        return {
+            'llave': value.name,
+            'valor': value.value
+        }
+
+
+class CancionSchema(SQLAlchemyAutoSchema):
+    class Meta:
+         model = Cancion
+         include_relationships = True
+         load_instance = True
+
+class AlbumSchema(SQLAlchemyAutoSchema):
+    medio = EnumADiccionario(attribute=("medio"))
+    class Meta:
+         model = Album
+         include_relationships = True
+         load_instance = True
+
+class UsuarioSchema(SQLAlchemyAutoSchema):
+    class Meta:
+         model = Usuario
+         include_relationships = True
+         load_instance = True
